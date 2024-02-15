c213d6bc2d9744317f8af6477b5c9530445f8c0d
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-15 15:26:49 +0700

    chore: adding product list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Carousel from "@/components/carousel";
+import { ProductList } from "@/components/list";
+import { PRODUCTS } from "@/lib/data/dummy";
 
 export default function Home() {
   return (
     <main className="w-full flex flex-col gap-4 px-12 py-4">
       <Carousel />
+      <ProductList products={[...PRODUCTS, ...PRODUCTS]} />
     </main>
   );
 }
