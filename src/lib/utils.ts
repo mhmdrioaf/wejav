88c1c0382993ae416db48a9105e28868fc3977f8
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-17 17:31:54 +0700

    chore: adding images placeholder utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { TProduct } from "@/types/globals";
-
 export function formatToIDR(value: number) {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
@@ -15,4 +13,28 @@ export function getProductURL(product: TProduct) {
     .join("-");
   const url = `/product/${productSlug}-${product.id}`;
   return url;
+}
+
+export const shimmerEffect = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#cfcfcf" offset="20%" />
+      <stop stop-color="#bababa" offset="50%" />
+      <stop stop-color="#cfcfcf" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#cfcfcf" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+export function toBase64(str: string) {
+  return typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+}
+
+export function generateShimmer(width: number, height: number) {
+  return `svg+xml;base64,${toBase64(shimmerEffect(width, height))}`;
 }
