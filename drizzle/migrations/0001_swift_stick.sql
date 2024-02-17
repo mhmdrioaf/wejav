3af9101fad5a1204d7996b636948a96dace8170c
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-17 10:33:53 +0700

    chore: uninstalling drizzle

--- drizzle/migrations/0001_swift_stick.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "product" ALTER COLUMN "seller_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "user_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "user_id" SET DATA TYPE text;
