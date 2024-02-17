20eaf44404fa454bccf03f44b2093abe92ec3e29
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-17 07:56:09 +0700

    chore: adding product url generator function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,18 @@
+import { TProduct } from "@/types/globals";
+
 export function formatToIDR(value: number) {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
     minimumFractionDigits: 0,
   }).format(value);
+}
+
+export function getProductURL(product: TProduct) {
+  const productTitles = product.title.split(" ").slice(0, 4);
+  const productSlug = productTitles
+    .map((slug) => slug.toLocaleLowerCase())
+    .join("-");
+  const url = `/product/${productSlug}-${product.id}`;
+  return url;
 }
