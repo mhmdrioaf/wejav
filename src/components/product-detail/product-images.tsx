cbd9fffceb04af51d4ffdbd847c277c2c96a448c
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-02-18 19:17:31 +0700

    chore: implementing product images modal state handler

--- src/components/product-detail/product-images.tsx
+++ src/components/product-detail/product-images.tsx
@@ -2,15 +2,21 @@
 
 import { generateShimmer } from "@/lib/utils";
 import Image from "next/image";
+import { ProductContext, TProductContext } from "./useProduct";
+import React from "react";
 
 interface IProductImagesProps {
   images: string[];
 }
 
 export default function ProductImages({ images }: IProductImagesProps) {
+  const { handler } = React.useContext(ProductContext) as TProductContext;
   return (
     <div className="w-full flex flex-col gap-4">
-      <div className="w-full h-auto aspect-square rounded-[32px] overflow-hidden relative">
+      <div
+        className="w-full h-auto aspect-square rounded-[32px] overflow-hidden relative cursor-pointer"
+        onClick={() => handler.onShowImagesModal(images[0])}
+      >
         <Image
           src={images[0]}
           fill
@@ -25,7 +31,8 @@ export default function ProductImages({ images }: IProductImagesProps) {
         {[...images, ...images].map((image, index) => (
           <div
             key={index}
-            className="w-[calc((100%-1rem)/3)] h-auto aspect-square rounded-2xl overflow-hidden shrink-0 relative"
+            className="w-[calc((100%-1rem)/3)] h-auto aspect-square rounded-2xl overflow-hidden shrink-0 relative cursor-pointer"
+            onClick={() => handler.onShowImagesModal(image)}
           >
             <Image
               src={image}
